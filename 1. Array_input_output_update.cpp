46564065e91e9305ec79e2068c7821722ba6de63
Rohit Balage <thatcomputerboy26@gmail.com>
2023-12-28 18:45:48 -0500

    added gitignore file to root location

---
+++ 1. Array_input_output_update.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int marks[100];
+int n;
+
+cout <<"Enter the no of students";
+cin >>n;
+
+for(int i =0; i<n; i++){
+cin >> marks[i];
+}
+
+for (int i=0; i<n; i++)
+{
+   cout <<"The students are: "<<marks[i]<< endl; 
+}
+
+}
