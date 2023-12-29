f9530b8a18c449ac7546ab24a4cf16da8deacd33
Rohit Balage <thatcomputerboy26@gmail.com>
2023-12-28 19:05:23 -0500

    1st file is completed

--- 1. Array_input_output_update.cpp
+++ 1. Array_input_output_update.cpp
@@ -3,19 +3,38 @@ using namespace std;
 
 int main()
 {
-int marks[100];
+int numbers[100];
 int n;
 
-cout <<"Enter the no of students";
+cout <<"Enter the total no. ";
 cin >>n;
 
+ // input
 for(int i =0; i<n; i++){
-cin >> marks[i];
+cin >> numbers[i];
 }
 
+// output
 for (int i=0; i<n; i++)
 {
-   cout <<"The students are: "<<marks[i]<< endl; 
+   cout <<"the entered no. are: "<<numbers[i]<< endl; 
 }
 
+//update
+
+int m, r;
+cout <<"\n Now Enter the index you want to change and folled by its the value";
+
+cin >> m >>r;
+
+numbers[m] = r;
+
+cout <<"the changed index is "<<m + " "<<"and the corresponding value is" <<numbers[m] + " = "<<r;
+
+cout <<"Now the changed numbers index would look like:";
+
+for(int i=0; i<=5; i++)
+{
+    cout <<numbers[i] <<endl;
+}
 }
