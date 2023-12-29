63ea0cd9c2da747b3c794b9f4031f65fe61d54a6
Rohit Balage <thatcomputerboy26@gmail.com>
2023-12-29 06:28:23 -0500

    6. Array elements pairing

---
+++ 6. printing_pairs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void printAllPairs(int arr[], int n)
+{
+   for(int i=0; i<n; i++)
+   {
+    int x= arr[i];
+    for(int j=i+1; j<n; j++)
+    {
+        int y = arr[j];  
+        cout << x << "," << y << endl;
+    }
+    cout << endl;
+   }
+}
+int main()
+{
+
+int arr[] ={ 10,20,30,40,50,60};
+int n = sizeof(arr)/sizeof(int);
+// print all pairs
+
+printAllPairs(arr, n);
+
+return 0;
+}
