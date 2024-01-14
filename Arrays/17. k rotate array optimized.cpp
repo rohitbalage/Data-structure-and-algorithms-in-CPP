44359e4556764d453c6e6a32d1ecfd3e39b0615f
Rohit Balage <thatcomputerboy26@gmail.com>
2024-01-14 15:33:35 -0500

    added 17. k rotate array optimized

---
+++ Arrays/17. k rotate array optimized.cpp
@@ -0,0 +1,49 @@
+/*
+K-Rotate
+Given an integer vector and a value k, your task is to rotate the array k times clockwise.
+
+Input Format
+
+In the function an integer vector and number k is passed.
+
+Output Format
+
+Return an integer vector.
+
+
+
+Sample Input
+
+{1, 3, 5, 7, 9}, x = 2
+
+
+Sample Output
+
+{7, 9, 1, 3, 5}
+
+
+Explanation
+
+After 1st rotation - {9, 1, 3, 5, 7}
+After 2nd rotation - {7, 9, 1, 3, 5}
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> kRotate(vector<int> arr, int k)
+{
+
+    // your code goes here:
+
+    int n = arr.size();
+
+    k = k % n;
+
+    reverse(arr.begin(), arr.begin() + n - k);
+    reverse(arr.begin() + n - k, arr.end());
+    reverse(arr.begin(), arr.end());
+
+    return arr;
+}
