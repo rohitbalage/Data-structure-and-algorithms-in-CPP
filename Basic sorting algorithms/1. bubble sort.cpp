4d6ccd8239d41442f61ed30a06dd9da72e03fcc0
Rohit Balage <thatcomputerboy26@gmail.com>
2024-01-14 16:00:37 -0500

    added 1. bubble sort and git ignore to json files

---
+++ Basic sorting algorithms/1. bubble sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int a[], int n)
+{
+    for (int times = 1; times <= n - 1; times++)
+    {
+        // repeated swapping
+        for (int j = 0; j <= n - times - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    int arr[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
+
+    int n = sizeof(arr) / sizeof(int);
+    bubble_sort(arr, n);
+
+    for (auto x : arr)
+    {
+        cout << x << ",";
+    }
+    return 0;
+}
