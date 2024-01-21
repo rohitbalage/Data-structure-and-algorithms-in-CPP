e8977d399b26d570e862b33066f7ae19af6bb6d5
Rohit Balage <thatcomputerboy26@gmail.com>
2024-01-21 11:52:46 -0500

    added 1. vector.cpp and enabled draw

---
+++ 6. vector data structure/1. vector.cpp
@@ -0,0 +1,17 @@
+/*
+1. vectors are sequence "containers" representing arrays that can
+change the size.
+2. just like arrays, vectors use contiguous storage locations for their elements, which means that their
+elements can also be accessed directly in O(1) time.
+
+3. but unlike arrays, their size can change dynamically, with theier storage being handled automatically
+by the container.
+
+![](1. vector.cp\ed2b4fd0-1bb7-e365-e16f-cdfede29a095.svg)
+
+1. How? --->  fundamentals
+
+2. Use?  -----> problems
+
+
+*/
