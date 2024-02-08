0a48c11aff16e07c2da592d4a261e3b1fd939b0c
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-08 18:46:56 -0500

    added 13. tilling problem

---
+++ 8. Recursion Basics/13. problem Tilling.cpp
@@ -0,0 +1,29 @@
+/*
+Tiling Problem!
+You are given N tiles of size 1 x M, There is a floor of size N x M which you have to cover with tiles. Find the number of ways the floor can be completely covered if you can place the tiles in both horizontal and vertical manner.
+
+
+
+Input Format:
+
+In the function, two integers N and M are passed.
+
+
+
+Output Format:
+
+Return a single integer denoting the number of ways.
+
+
+
+Sample Input:
+
+4 3
+
+
+
+Sample Output:
+
+3
+
+*/
