7e964156917c2c7233785c883c83915982e4268d
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-14 18:31:08 -0500

    added 1. backtracking introduction

---
+++ 10. backtracking/1. backtracking introduction.cpp
@@ -0,0 +1,21 @@
+/*
+Backtracking: algorithmic technique in which we recursively try to build the solutions
+
+-It is an alorithmic technique
+-Recursively trying to build  a solution
+-Searching the entire search space to solve computation problem
+
+
+Problem types:
+Decision problem: check for feasible solution
+Optimization problem: find best solutions
+Enumeraion problem:  find all solutions
+
+
+classical problems:
+1. rat in maze
+2. N queen problem
+3. sudoku solver
+4. knights tour problem
+
+*/
