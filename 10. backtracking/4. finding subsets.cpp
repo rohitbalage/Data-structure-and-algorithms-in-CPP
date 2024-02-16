a08153094baff519a072077a1ee3c01f3f924393
Rohit Balage <thatcomputerboy26@gmail.com>
2024-02-15 21:26:12 -0500

    added 1. space and time complexity

---
+++ 10. backtracking/4. finding subsets.cpp
@@ -0,0 +1,17 @@
+/*
+Finding subsets:
+
+Given a string, find all the subsets of the given string
+
+
+input:
+abc
+
+2*2*2 = 8 options
+
+output:
+
+"",a,b,c,ab,ac,bc,abc
+
+*/
+
---
+++ 11. space and time complexity analysis/1. space and time complexity.cpp
@@ -0,0 +1,29 @@
+/*
+Theoretical analysis
+
+both time and space are going to be some 
+function of input.
+
+Time   ^   f(n)   >> quadratic, linear, constant
+                        N^2       
+space  ^   f(n)   >> 
+
+
+for eg.
+
+N -10N
+T -10^2T   
+
+Notation:
+
+O(N^2)
+
+Time = CN^2
+
+
+Q. find largest element in a sorted array.
+ >> we know the largest element is last, so we 
+ can find this in constant time. O(n)
+ 
+
+*/
