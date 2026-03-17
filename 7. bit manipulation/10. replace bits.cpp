a0f32053d3d65f6d192494abbefa9e38196106c0
Rohit Balage <thatcomputerboy26@gmail.com>
2026-03-16 21:14:59 -0400

    added 10. replace bits

--- 7. bit manipulation/10. replace bits.cpp
+++ 7. bit manipulation/10. replace bits.cpp
@@ -16,10 +16,29 @@ Output: 1001010100
 
 */
 
+void clearIBitsInRange(int &n, int i, int j)
+{
+   int a= (~0)<<(j+1);
+   int b=(1<<i) -1;
+   int mask=a|b;
+   n=n &mask;
+}
+
+void replaceBits(int &n, int i, int j, int m)
+{
+    clearIBitsInRange(n,i,j);
+    int mask=(m<<i);
+    n = n| mask;
+}
+
 int main()
 {
+int n=15;
+int i=1;
+int j=3;
+int m=2;
 
-
-
+replaceBits(n,i,j,m);
+cout << n<<endl;
 return 0;
 }
