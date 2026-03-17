f2dedbef11d1d2fe9784d5bf3712097141655ee8
Rohit Balage <thatcomputerboy26@gmail.com>
2026-03-16 21:38:27 -0400

    added 12. set bits

---
+++ 7. bit manipulation/12. count number of set bits.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+
+*/
+
+int count_bits(int n)
+{
+    int count =0;
+    while(n>0)
+    {
+        int last_bit = (n&1);
+        count +=last_bit;
+        n= n>>1;
+    }
+    return count;
+}
+
+int main()
+
+{
+    int n;
+    cin >>n;
+    cout <<count_bits(n)<<endl;
+
+    return 0;
+}
