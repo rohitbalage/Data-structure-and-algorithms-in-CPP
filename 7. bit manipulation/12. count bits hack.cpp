d7889858408a79d621f412673127993cb6ec5a60
Rohit Balage <thatcomputerboy26@gmail.com>
2026-03-16 21:48:24 -0400

    added 12. count bit hack

---
+++ 7. bit manipulation/12. count bits hack.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+
+*/
+
+int count_bits(int n)
+{
+    int count =0;
+    while(n>0)
+    {
+        int last_bit = (n&1);
+        count +=last_bit;
+        n= n>>1;
+    }
+    return count;
+}
+
+// counting sets bits:; hack (faster method)
+int count_bits_hack(int n)
+{
+    int ans=0;
+    while(n>0)
+    // removes the last set bit from the current number
+    {
+        n =n & (n-1);
+        ans++;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin >>n;
+    cout <<count_bits(n)<<endl;
+    cout <<count_bits_hack(n)<<endl;
+    return 0;
+}
