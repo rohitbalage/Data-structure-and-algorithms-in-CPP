bb14103b2e628b7c4394188ca9cc2bc788f994f8
Rohit Balage <thatcomputerboy26@gmail.com>
2026-03-24 18:27:25 -0400

    added 4. finding subsets with backtracing

--- 10. backtracking/4. finding subsets.cpp
+++ 10. backtracking/4. finding subsets.cpp
@@ -3,7 +3,6 @@ Finding subsets:
 
 Given a string, find all the subsets of the given string
 
-
 input:
 abc
 
@@ -15,3 +14,41 @@ output:
 
 */
 
+#include <iostream>
+using namespace std;
+
+void findSubsets(char *input, char *output, int i, int j) {
+    // Base Case: We've reached the end of the input string
+    if (input[i] == '\0') {
+        output[j] = '\0'; // Terminate the output string
+        if (j == 0) {
+            cout << "{empty}" << endl;
+        } else {
+            cout << output << endl;
+        }
+        return;
+    }
+
+    // Recursive Case
+    // Option 1: Include the current character
+    output[j] = input[i];
+    findSubsets(input, output, i + 1, j + 1);
+
+    // Option 2: Exclude the current character
+    // We don't increment 'j', so the next character we "include" 
+    // will just overwrite the current spot.
+    findSubsets(input, output, i + 1, j);
+}
+
+int main() {
+    char input[100];
+    char output[100];
+
+    cout << "Enter a string (e.g., abc): ";
+    cin >> input;
+
+    cout << "--- Subsets ---" << endl;
+    findSubsets(input, output, 0, 0);
+
+    return 0;
+}
